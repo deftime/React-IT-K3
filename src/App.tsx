990b80739316c4e6aa5b4a0b8cf2dc410139e27f
Default <deftime@gmail.com>
2025-11-20 21:44:56 +0200

    HW4, ToDoList, begin.

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,25 @@ import './App.css'
 
 function App() {
 
+  const tasks = [
+    { id: 1, title: "Купить продукты на неделю", isDone: false },
+    { id: 2, title: "Полить цветы", isDone: true },
+    { id: 3, title: "Сходить на тренировку", isDone: false },
+  ]
+
   return (
     <>
-
+      <h1 className="title">To Do List</h1>
+      <ul>
+        {tasks.map((task) => {
+          return (
+            <li key={task.id}>
+              <input type="checkbox" disabled checked={task.isDone} />
+              <div>{task.title}</div>
+            </li>
+          )
+        })}
+      </ul>
     </>
   )
 }

51add52be2ca53ca468b7a40b51c435949abb144
Default <deftime@gmail.com>
2025-11-22 19:50:58 +0200

    Connect Data and Interface. None, Loading, Selected.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,48 @@
 import './App.css'
 
-function App() {
+//const tracks = null;
+const tracks = [
+    {
+        id: 1,
+        title: "Musicfun soundtrack",
+        url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3",
+    },
+    {
+        id: 2,
+        title: "Musicfun soundtrack instrumental",
+        url: " https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3",
+    },
+]
 
-  return (
-    <>
+const selectedTrack = 2;
 
-    </>
-  )
+export function App() {
+
+    // if (tracks.length === 0) {
+    //     return (
+    //         <div className="none">No tracks!</div>
+    //     )
+    // }
+
+    return (
+        <section className="music-player">
+            <h1 id={"title"} className={"title"}>
+                My player
+            </h1>
+            <hr/>
+            <div className="track-box">
+                {!tracks ? <span>Loading...</span> : undefined}
+                {tracks && tracks.length === 0 ? <span>NO tracks!</span> : tracks?.map((item) => {
+                    return (
+                        <div key={item.id} className="track" style={{
+                            backgroundColor: item.id === selectedTrack ? 'yellow' : 'none'
+                        }}>
+                            <div className="name">{item.title}</div>
+                            <audio src={item.url} controls={true}/>
+                        </div>
+                    )
+                })}
+            </div>
+        </section>
+    )
 }
-
-export default App

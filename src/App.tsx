f13c93ee25334c5a0de1b7e77ebde6acdf26e5d5
Default <deftime@gmail.com>
2025-11-23 17:12:41 +0200

    Run useState()

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,53 @@
 import './App.css'
+import { useState } from "react";
 
 //const tracks = null;
 const tracks = [
     {
         id: 1,
-        title: "Musicfun soundtrack",
+        title: "MusicFun MainTheme",
         url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3",
     },
     {
         id: 2,
-        title: "Musicfun soundtrack instrumental",
+        title: "MusicFun Soundtrack",
+        url: " https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3",
+    },
+    {
+        id: 3,
+        title: "MusicFun MT",
         url: " https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3",
     },
 ]
 
-const selectedTrack = 2;
-
 export function App() {
 
-    // if (tracks.length === 0) {
-    //     return (
-    //         <div className="none">No tracks!</div>
-    //     )
-    // }
+    const [selectedTrack, setSelected] = useState(null);
 
     return (
-        <section className="music-player">
-            <h1 id={"title"} className={"title"}>
+        <section className={'music-player'}>
+            <h1 id={'title'} className={'title'}>
                 My player
             </h1>
             <hr/>
-            <div className="track-box">
+            <div className={'track-box'}>
                 {!tracks ? <span>Loading...</span> : undefined}
                 {tracks && tracks.length === 0 ? <span>NO tracks!</span> : tracks?.map((item) => {
                     return (
-                        <div key={item.id} className="track" style={{
-                            backgroundColor: item.id === selectedTrack ? 'yellow' : 'none'
-                        }}>
-                            <div className="name">{item.title}</div>
+                        <div key={item.id} className={'track'} style={{
+                            background: item.id === selectedTrack ? 'yellow' : 'none'
+                        }} onClick={ ()=>{
+                            setSelected(item.id)
+                        } }>
+                            <div className={'name'}>{item.title}</div>
                             <audio src={item.url} controls={true}/>
                         </div>
                     )
                 })}
             </div>
+            <div className={'btn-reset'} onClick={()=>{
+                setSelected(null)
+            }}>Reset</div>
         </section>
     )
 }

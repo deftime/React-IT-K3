b7b4c791d97db6594fc555e1cb0677dc7f2f6dae
Default <deftime@gmail.com>
2025-11-23 14:08:31 +0200

    HW8, Data and View.

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,58 @@ import './App.css'
 
 function App() {
 
-  const tasks = [
-    { id: 1, title: "Купить продукты на неделю", isDone: false },
-    { id: 2, title: "Полить цветы", isDone: true },
-    { id: 3, title: "Сходить на тренировку", isDone: false },
-  ]
+    //const tasks = null;
+    //const tasks = [];
+    const tasks = [
+        {
+            id: 1,
+            title: "Buy some eats",
+            isDone: true,
+            addedAt: "01.09.2025",
+            priority: 1
+        },
+        {
+            id: 2,
+            title: "Flow flowers",
+            isDone: true,
+            addedAt: "02.09.2025",
+            priority: 2
+        },
+        {
+            id: 3,
+            title: "Go out",
+            isDone: false,
+            addedAt: "03.09.2025",
+            priority: 3
+        },
+    ]
 
-  return (
-    <>
-      <h1 className="title">To Do List</h1>
-      <ul>
-        {tasks.map((task) => {
-          return (
-            <li key={task.id}>
-              <input type="checkbox" disabled checked={task.isDone} />
-              <div>{task.title}</div>
-            </li>
-          )
-        })}
-      </ul>
-    </>
-  )
+    return (
+        <div className={'toDoList main'}>
+            <h1 className={'title h1'}>To Do List</h1>
+            <div className={'list-box'}>
+                {!tasks ? <span>Loading...</span> : undefined}
+                {tasks && tasks.length === 0 ? <span>No tasks</span> : tasks?.map((task) => {
+                    return (
+                        <div className={'task task-' + task.priority} key={task.id}>
+                            <div className={'title line'}>
+                                <span>Task: </span>
+                                <span style={{ textDecoration: task.isDone ? 'line-through' : 'none' }}>{task.title}</span>
+                            </div>
+                            <div className={'status line'}>
+                                <span>Status: </span>
+                                <input type="checkbox" disabled checked={task.isDone}/>
+                            </div>
+                            <div className={'date line'}>
+                                <span>Date: </span>
+                                <span>{task.addedAt}</span>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
 }
 
 export default App

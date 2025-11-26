c298cbbbcfcafac3681bd8ddaffefbc06b32d394
Default <deftime@gmail.com>
2025-11-26 20:55:59 +0200

    Get remote data and useEffects.

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,49 @@
-import './App.css'
-import { useState } from "react";
-
-//const tracks = null;
-const tracks = [
-    {
-        id: 1,
-        title: "MusicFun MainTheme",
-        url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3",
-    },
-    {
-        id: 2,
-        title: "MusicFun Soundtrack",
-        url: " https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3",
-    },
-    {
-        id: 3,
-        title: "MusicFun MT",
-        url: " https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3",
-    },
-]
+import clsx from 'clsx';
+import cls from './scss/App.module.scss';
+import {useEffect, useState} from "react";
+import loader from './img/loader.svg';
 
 export function App() {
 
     const [selectedTrack, setSelected] = useState(null);
+    const [tracks, setTracks] = useState(null);
+
+    console.log(tracks);
+
+    useEffect(()=>{
+        fetch('https://musicfun.it-incubator.app/api/1.0/playlists/tracks', {
+            headers: {
+                'api-key': '***' // Set key
+            }
+        })
+            .then(resp => resp.json())
+            .then(json => {
+                setTracks(json.data)
+            })
+    }, []);
 
     return (
-        <section className={'music-player'}>
-            <h1 id={'title'} className={'title'}>
+        <section className={cls.musicPlayer}>
+            <h1 id={'title'} className={cls.title}>
                 My player
             </h1>
             <hr/>
-            <div className={'track-box'}>
-                {!tracks ? <span>Loading...</span> : undefined}
+            <div className={cls.trackBox}>
+                {!tracks ? <img src={loader} alt="" /> : undefined}
                 {tracks && tracks.length === 0 ? <span>NO tracks!</span> : tracks?.map((item) => {
                     return (
-                        <div key={item.id} className={'track'} style={{
+                        <div key={item.id} className={cls.track} style={{
                             background: item.id === selectedTrack ? 'yellow' : 'none'
                         }} onClick={ ()=>{
                             setSelected(item.id)
                         } }>
-                            <div className={'name'}>{item.title}</div>
-                            <audio src={item.url} controls={true}/>
+                            <div className={cls.name}>{item.attributes.title}</div>
+                            <audio src={item.attributes.attachments[0].url} controls={true}/>
                         </div>
                     )
                 })}
             </div>
-            <div className={'btn-reset'} onClick={()=>{
+            <div className={cls.btnReset} onClick={()=>{
                 setSelected(null)
             }}>Reset</div>
         </section>

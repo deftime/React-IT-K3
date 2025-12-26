099758fbfcee5ad47c725b562dc7b743a763c1c5
Default <deftime@gmail.com>
2025-12-26 19:41:59 +0200

    Get Detail of selected Track.

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,13 @@ import loader from './img/loader.svg';
 export function App() {
 
     const [selectedTrackId, setSelectedId] = useState(null);
+    const [selectedTrack, setSelected] = useState(null);
     const [tracks, setTracks] = useState(null);
 
     useEffect(() => {
         fetch('https://musicfun.it-incubator.app/api/1.0/playlists/tracks', {
             headers: {
-                'api-key': 'a34b9cfb-e9cc-46bc-8985-5f6746df6e8f' // Set key
+                'api-key': '***' // Set key
             }
         })
             .then(resp => resp.json())
@@ -22,6 +23,19 @@ export function App() {
 
     function selectTrack(id) {
         setSelectedId(id);
+
+        fetch('https://musicfun.it-incubator.app/api/1.0/playlists/tracks/' + id, {
+            headers: {
+                'api-key': '***' // Set key
+            }
+        })
+            .then(resp => resp.json())
+            .then(json => {
+                setSelected(json.data)
+            })
+
+        console.log(selectedTrack);
+
     }
 
     return (
@@ -38,7 +52,7 @@ export function App() {
                         return (
                             <div key={item.id} className={clsx(cls.track, item.id === selectedTrackId && cls.selected)}
                                  onClick={() => {
-                                     setSelectedId(item.id)
+                                     selectTrack(item.id)
                                  }}>
                                 <div className={cls.name}>{item.attributes.title}</div>
                                 <audio src={item.attributes.attachments[0].url} controls={true}/>
@@ -47,14 +61,22 @@ export function App() {
                     })}
                     <div className={cls.btnReset} onClick={() => {
                         setSelectedId(null)
+                        setSelected(null)
                     }}>Reset
                     </div>
                 </div>
                 <div className={clsx(cls.side, cls.details)}>
                     <div className={cls.sideTitle}>Details</div>
-                    <div className={cls.infoBox}>
-
-                    </div>
+                    {!selectedTrackId && <div className={cls.noTrack}>Select a track!</div>}
+                    {selectedTrackId && !selectedTrack && <img src={loader} alt="" className={cls.loader}/>}
+                    {selectedTrack && <div className={cls.infoBox}>
+                        <div className={cls.name}>{selectedTrack.attributes.title}</div>
+                        <div className={cls.date}>{new Date(selectedTrack.attributes.releaseDate).toLocaleDateString()}</div>
+                        <div className={cls.duration}>{selectedTrack.attributes.duration} min.</div>
+                        <div className={cls.size}>{selectedTrack.attributes.attachments[0].fileSize} Bytes</div>
+                        <hr/>
+                        <div className={cls.lyrics}>{selectedTrack.attributes.lyrics}</div>
+                    </div>}
                 </div>
             </div>
         </section>

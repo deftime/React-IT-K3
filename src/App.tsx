23ad8810d4053b1abb897ed1eeb541a2ea56639d
Default <deftime@gmail.com>
2025-12-27 15:45:11 +0200

    HW13, list and details

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,14 @@ import {useEffect, useState} from "react";
 
 function App() {
 
-    const [selected, setSelect] = useState(null);
+    const [selectedId, setSelectId] = useState(null);
+    const [selectedTask, setSelectTask] = useState(null);
     const [tasks, setTasks] = useState(null);
 
     useEffect(()=>{
         fetch('https://trelly.it-incubator.app/api/1.0/boards/tasks', {
             headers: {
-                'api-key': '***' // Set key
+                'api-key': 'c54c1881-2ff2-421a-bc1e-98d829a16de3' // Set key
             }
         })
             .then(res => res.json())
@@ -23,47 +24,80 @@ function App() {
     function getStatus(status) {
         switch (status) {
             case 0:
-                return (<div className={clsx(cls.plate, cls.todo)}>To Do</div>)
+                return (<div className={clsx(cls.statusPlate, cls.todo)}>To Do</div>)
             case 1:
-                return (<div className={clsx(cls.plate, cls.progress)}>In progress</div>)
+                return (<div className={clsx(cls.statusPlate, cls.progress)}>In progress</div>)
             case 2:
-                return (<div className={clsx(cls.plate, cls.done)}>Done</div>)
+                return (<div className={clsx(cls.statusPlate, cls.done)}>Done</div>)
             case 3:
-                return (<div className={cls.plate}>Draft</div>)
+                return (<div className={cls.statusPlate}>Draft</div>)
         }
+    }
+
+    function selectTask(taskId, boardId) {
+        setSelectId(taskId);
+
+        fetch(`https://trelly.it-incubator.app/api/1.0/boards/${boardId}/tasks/${taskId}`, {
+            headers: {
+                'api-key': 'c54c1881-2ff2-421a-bc1e-98d829a16de3' // Set key
+            }
+        })
+            .then(res => res.json())
+            .then(json => {
+                setSelectTask(json.data)
+            })
+
+        console.log(selectedTask);
     }
 
     return (
         <section className={cls.toDoList}>
             <h1 className={cls.mainTitle}>To Do List</h1>
-            <div className={cls.listBox}>
-                {!tasks ? <img src={loader} alt="" className={cls.loader}/> : undefined}
-                {tasks && tasks.length === 0 ? <span>No tasks</span> : tasks?.map((task) => {
-                    let select = task.id === selected;
-                    return (
-                        <div className={clsx(cls.task, select && cls.border, cls.task + task.priority)} key={task.id}
-                             onClick={()=>{
-                                setSelect(task.id)
-                             }}
-                        >
-                            <div className={clsx(cls.name, cls.line)}>
-                                <span style={{ textDecoration: task.attributes.status >= 2 ? 'line-through' : 'none' }}>{task.attributes.title}</span>
-                            </div>
-                            <div className={clsx(cls.status, cls.line)}>
-                                <span>Status: </span>
-                                <div className={cls.statusBox}>
+            <div className={cls.inner}>
+                <div className={cls.listBox}>
+                    {!tasks && <img src={loader} alt="" className={cls.loader}/>}
+                    {tasks && tasks.length === 0 ? <span>No tasks</span> : tasks?.map((task) => {
+                        return (
+                            <div className={clsx(cls.task, task.id === selectedId && cls.border, cls.task + task.priority)} key={task.id}
+                                 onClick={()=>{
+                                    setSelectTask(null)
+                                    selectTask(task.id, task.attributes.boardId)
+                                 }}
+                            >
+                                <div className={clsx(cls.name, cls.line)}>
+                                    <span style={{ textDecoration: task.attributes.status >= 2 ? 'line-through' : 'none' }}>{task.attributes.title}</span>
+                                </div>
+                                <div className={clsx(cls.status, cls.line)}>
+                                    <span>Status: </span>
                                     {getStatus(task.attributes.status)}
                                 </div>
+                                <div className={clsx(cls.data, cls.line)}>
+                                    <span>Date: </span>
+                                    <span>{new Date(task.attributes.addedAt).toLocaleDateString()}</span>
+                                </div>
                             </div>
-                            <div className={clsx(cls.data, cls.line)}>
-                                <span>Date: </span>
-                                <span>{new Date(task.attributes.addedAt).toLocaleDateString()}</span>
+                        )
+                    })}
+                </div>
+                <div className={cls.taskInfo}>
+                    {!selectedId && <div className={cls.noTask}>Select a task!</div>}
+                    {selectedId && !selectedTask && <img src={loader} alt="" className={cls.loader}/>}
+                    {selectedTask && <>
+                        <div className={cls.top}>
+                            <div className={cls.name}>{selectedTask.attributes.title}</div>
+                            <div className={cls.datas}>
+                                {getStatus(selectedTask.attributes.status)}
+                                <div className={cls.date}>{new Date(selectedTask.attributes.addedAt).toLocaleDateString()}</div>
                             </div>
                         </div>
-                    )
-                })}
+                        <div className={cls.desc}>{selectedTask.attributes.description ?? 'No description.'}</div>
+                    </>}
+                </div>
             </div>
-            <div className={cls.btnReset} onClick={()=>{setSelect(null)}}>Reset</div>
+            <div className={cls.btnReset} onClick={()=>{
+                setSelectId(null)
+                setSelectTask(null)
+            }}>Reset</div>
         </section>
     )
 }

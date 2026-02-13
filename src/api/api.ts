2ba74face2c70b7d49bdb272d8bee38a70d23b87
Default <deftime@gmail.com>
2026-02-13 20:46:28 +0200

    Create environment and hide key.

--- src/api/api.ts
+++ src/api/api.ts
@@ -14,33 +14,17 @@ const api = {
     getTasks(): Promise<{data: TaskType[]}> {
         return fetch('https://trelly.it-incubator.app/api/1.0/boards/tasks', {
             headers: {
-                'api-key': '7125e182-3d06-4aff-82e8-ebec790f10eb' // Set key
+                'api-key': import.meta.env.VITE_API_KEY
             }
         }).then(res => res.json())
     },
     getTask(boardId: string | null, taskId: string | null): Promise<{data: TaskType}> {
         return fetch(`https://trelly.it-incubator.app/api/1.0/boards/${boardId}/tasks/${taskId}`, {
             headers: {
-                'api-key': '7125e182-3d06-4aff-82e8-ebec790f10eb' // Set key
+                'api-key': import.meta.env.VITE_API_KEY
             }
         }).then(res => res.json())
     }
 }
 
 export default api;
-
-// export function getTasks(): Promise<{data: TaskType[]}> {
-//     return fetch('https://trelly.it-incubator.app/api/1.0/boards/tasks', {
-//         headers: {
-//             'api-key': '7125e182-3d06-4aff-82e8-ebec790f10eb' // Set key
-//         }
-//     }).then(res => res.json())
-// }
-//
-// export function getTask(boardId: string, taskId: string): Promise<{data: TaskType}> {
-//     return fetch(`https://trelly.it-incubator.app/api/1.0/boards/${boardId}/tasks/${taskId}`, {
-//         headers: {
-//             'api-key': '7125e182-3d06-4aff-82e8-ebec790f10eb' // Set key
-//         }
-//     }).then(res => res.json())
-// }

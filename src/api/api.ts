5cb41106a8138347166fe9c810fc17b92a5b67c6
Default <deftime@gmail.com>
2026-02-13 20:49:03 +0200

    Keys to .env!

--- src/api/api.ts
+++ src/api/api.ts
@@ -17,7 +17,7 @@ export type TrackType = {
 export function getTracks(): Promise<{data: TrackType[]}> {
     return fetch('https://musicfun.it-incubator.app/api/1.0/playlists/tracks', {
         headers: {
-            'api-key': 'e514685f-7e59-43d6-9938-2734fd22d550' // Set key
+            'api-key': import.meta.env.VITE_API_KEY_PLAYER
         }
     }).then(resp => resp.json())
 }
@@ -25,7 +25,7 @@ export function getTracks(): Promise<{data: TrackType[]}> {
 export function getTrack(id: string): Promise<{data: TrackType}> {
     return fetch('https://musicfun.it-incubator.app/api/1.0/playlists/tracks/' + id, {
         headers: {
-            'api-key': 'e514685f-7e59-43d6-9938-2734fd22d550' // Set key
+            'api-key': import.meta.env.VITE_API_KEY_PLAYER
         }
     }).then(resp => resp.json())
 }
